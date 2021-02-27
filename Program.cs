cb3f40e9a1f1ac1473e10bc201eedb5cbbf618e6
2017247946 <2017247946@qq.com>
2021-02-27 18:54:41 +0800

    finish simple

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace csharp_hw_1._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a,b,c,d = 0;
+            Console.WriteLine("please input the first number:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("please input the second number:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("please input the sign:");
+            c = Console.Read();
+            switch (c)
+            {
+                case 42: d = a * b; break;
+                case 43: d = a + b; break;
+                case 45: d = a - b; break;
+                case 47: d = a / b; break;
+                default: break;
+            }
+            Console.WriteLine("the answer is : {0}",d);
         }
     }
 }
